b5dd05b30a0bfcaaeb52b4d99cdb5d5cf7b8e127
kosta <kosta@kosta-PC>
2016-07-01 10:27:08 +0900

    Jenkins webhook

--- src/main/java/kr/co/kosta/hello_maven2/App.java
+++ src/main/java/kr/co/kosta/hello_maven2/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Mr. SkdocX!" );
+        System.out.println( "Hello Mr. SkdocX !!!" );
+        System.out.println( "Hello Jenkins Webhook !!!" );
     }
 }
